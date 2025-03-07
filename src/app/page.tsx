4599bcc1b4176c123c7c617a3a27aaa1c590958f
Business Konto <Businesskonto@Mac.fritz.box>
2025-03-08 00:12:24 +0100

    fix: remove unused Image import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 bg-black overflow-hidden">
